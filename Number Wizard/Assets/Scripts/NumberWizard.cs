752807308a6a673e9af3ae89d24a7d154772e4c8
Adam <AdamFraserPrograming@gmail.com>
2017-11-25 17:15:22 +0000

    new laptop update 2

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+	int maxGuess;
+	int minGuess;
+	int guess;
+
+	void Start () {
+		startGame();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//up arrow
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			minGuess = guess;
+			nextGuess();
+		}
+		//down arrow
+		else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			maxGuess = guess;
+			nextGuess();
+		}
+		//return
+		else if(Input.GetKeyDown(KeyCode.Return)){
+			print("YAY I WON XD");
+			startGame();//restarts game once PC has won
+		}
+	}
+
+	void startGame() {
+		guess = 1000000 / 2;
+		minGuess = 0;
+		maxGuess = 1000000;
+
+		nextGuess();
+
+		print("Welcome to Number Wizard!");
+		print("Pick a number in your head but don't tell me what it is.");
+
+		print("The highest number you can pick is " + maxGuess);
+		print("The lowest number you can pick is " + minGuess);
+
+		print("is the number higher or lower or higher than " + guess + "?");
+		print("up arrow for higher, down arrow for lower, return for correct guess!");
+
+		maxGuess = maxGuess + 1; //avoids max guess caping at max - 1 due to rounding
+	}
+
+	void nextGuess() {
+		guess = (maxGuess + minGuess) / 2;
+
+		print("is the number higher or lower or higher than " + guess + "?");
+		print("up arrow for higher, down arrow for lower, return for correct guess!");
+	}
+}
---
+++ Pong/Assets/Ball.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+    int scoreLeft, scoreRight = 0;
+
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                    float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "racketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+            
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "racketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        //score checkers
+        if (col.gameObject.name == "wallLeft") {
+            scoreRight++;
+            GameObject.Find("txtScoreRight").GetComponent<Text>().text = scoreRight.ToString();            
+        } else if (col.gameObject.name == "wallRight"){
+            scoreLeft++;
+            GameObject.Find("txtScoreLeft").GetComponent<Text>().text = scoreLeft.ToString();
+        }
+    }
+}
